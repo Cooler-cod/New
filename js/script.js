17e49d10d2f8538a6e8e4a72f262047587b7b5b7
Andrii <evtushenko@ukr.net>
2021-05-05 16:53:28 +0300

    second task

--- js/script.js
+++ js/script.js
@@ -16,11 +16,33 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-    b = prompt('На сколько оцениваете его?'),
-    c = prompt('Один из последних просмотреных фильмов?', ''),
-    d = prompt('На сколько оцениваете его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 1; i <= 2; i++) {
+    const a = prompt('Один из последних просмотреных фильмов?', ''),
+        b = prompt('На сколько оцениваете его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+if (personalMovieDB.count < 10) {
+    alert('you have watched few movies');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert('you are a classic spectator');
+} else if (personalMovieDB > 30) {
+    alert('you are movie buff');
+} else {
+    alert('error');
+}
+
+// const a = prompt('Один из последних просмотреных фильмов?', ''),
+//     b = prompt('На сколько оцениваете его?'),
+//     c = prompt('Один из последних просмотреных фильмов?', ''),
+//     d = prompt('На сколько оцениваете его?');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 console.log(personalMovieDB.movies);
